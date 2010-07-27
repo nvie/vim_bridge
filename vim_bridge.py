c7073bdb4be08a2f695614da06bbde33e5943486
Vincent Driessen <vincent@datafox.nl>
2010-07-27 22:48:17 +0200

    Initial commit.

---
+++ vim_bridge.py
@@ -0,0 +1,23 @@
+func_register = {}
+
+class bridged(object):
+
+    def __init__(self, *args):
+        self.args = args
+
+    def __call__(self, fin):
+        func_register[fin.func_name] = fin
+
+        import vim
+        lines = ['function %s(%s)' % (fin.func_name, ", ".join(self.args))]
+        lines.append('python << endp')
+        for arg in self.args:
+            lines.append('__%s = vim.eval("a:%s")' % (arg, arg))
+        lines.append('__result = func_register["%s"](%s)' % (fin.func_name, ", ".join(map(lambda s: "__%s" % s, self.args))))
+        lines.append('vim.command("return %s" % repr(__result))')
+        lines.append('endp')
+        lines.append('endf')
+        vim.command("\n".join(lines))
+
+        return fin
+
